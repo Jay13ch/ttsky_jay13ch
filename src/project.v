b788a754ecb1b05b7c3fc749cd82fd4361fba657
Jay13ch <jaya.ece@cambridge.edu.in>
2026-03-12 13:33:26 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_jay13ch_vga_matrix_dac (
+module tt_um_algofoogle_vga_matrix_dac (
     input  wire       VGND,
     input  wire       VDPWR,    // 1.8v power supply
 //    input  wire       VAPWR,    // 3.3v power supply
